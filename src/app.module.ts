49a838add28037908c5dbfd2c1f14dc681d61d2c
Zhanibek <zkhodzhabekov@gmail.com>
2023-03-16 14:43:54 +0600

    Changed return type of messages to objects in delete update and create User methods, changed return type to objects instead of string

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ envFilePath: '.env' }), PrismaModule],
+  imports: [AuthModule, ConfigModule.forRoot({ envFilePath: '.env' }), PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,115 +7,107 @@ import { prisma } from '@prisma/client';
 @Injectable()
 export class UserService {
     
-    constructor(private prismaService: PrismaService){}
+  constructor(private prismaService: PrismaService){}
 
-    async encryptPassword(password: string){
-      const saltRounds: number = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10);
-      const salt = bcrypt.genSaltSync(saltRounds)
-      const encryptedPassword: string = await bcrypt.hash(password, salt)
-      return encryptedPassword;
+  async encryptPassword(password: string){
+    const saltRounds: number = parseInt(process.env.BCRYPT_SALT_ROUNDS, 10);
+    const salt = bcrypt.genSaltSync(saltRounds)
+    const encryptedPassword: string = await bcrypt.hash(password, salt)
+    return encryptedPassword;
+  }
+
+  async getAllUsers(){
+    return await this.prismaService.user.findMany({
+      select:{
+        id: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        phone: true
+      }
+    });
+  }
+
+  async getUser(userId: number){
+    const user = await this.prismaService.user.findFirst({
+      select:{
+        id: true,
+        email: true,
+        firstname: true,
+        lastname: true,
+        phone: true
+      },
+      where: {
+        id: userId
+      }
+    });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  async createUser(userDto: CreateDto){
+    const user = await this.prismaService.user.findUnique({
+      where: { 
+        email: userDto.email 
+      }
+    });
+    if (user) {
+      throw new ForbiddenException('Email already in use');
     }
 
-    async getAllUsers(){
-      return await this.prismaService.user.findMany({
-        select:{
-          id: true,
-          email: true,
-          firstname: true,
-          lastname: true,
-          phone: true
-        }
-      });
+    let encryptedPassword: string = await this.encryptPassword(userDto.password)
+
+    const newUser = await this.prismaService.user.create({
+      data: {
+        email: userDto.email,
+        password: encryptedPassword,
+        firstname: userDto.firstname,
+        lastname: userDto.lastname,
+        phone: userDto.phone
+      }
+    });
+    return {
+      message: `Successfully created user`,
+      user: newUser
+    };
+  }
+
+  async updateUser(userId: number, userDto: CreateDto) {
+    let encryptedPassword;
+    try{
+        encryptedPassword = this.encryptPassword(userDto.password)
+    }
+    catch(err){
+      return {message: err};
     }
 
-    async getUser(userId: number){
-      const user = await this.prismaService.user.findFirst({
-        select:{
-          id: true,
-          email: true,
-          firstname: true,
-          lastname: true,
-          phone: true
-        },
-        where: {
-          id: userId
-        }
-      });
-      if (!user) {
-        throw new NotFoundException('User not found');
+    const newUser = await this.prismaService.user.update({
+      where: {
+        id: userId
+      },
+      data: {
+        email: userDto.email,
+        password: encryptedPassword,
+        firstname: userDto.firstname,
+        lastname: userDto.lastname,
+        phone: userDto.phone
       }
-      return user;
-    }
+    });
+    return {
+      message: `Successfully updated user with id: ${userId}`,
+      user: newUser
+    };
+  }
 
-    async createUser(userDto: CreateDto){
-      const user = await this.prismaService.user.findUnique({
-        where: { 
-          email: userDto.email 
-        }
-      });
-      if (user) {
-        throw new ForbiddenException('Email already in use');
+  async deleteUser(userId: number){
+    await this.prismaService.user.delete({
+      where: {
+        id: userId
       }
-
-      let encryptedPassword;
-      try{
-         encryptedPassword = this.encryptPassword(userDto.password)
-      }
-      catch(err){
-        return err;
-      }
-
-      const newUser = await this.prismaService.user.create({
-        data: {
-          email: userDto.email,
-          password: encryptedPassword,
-          firstname: userDto.firstname,
-          lastname: userDto.lastname,
-          phone: userDto.phone
-        }
-      });
-      return newUser;
-    }
-
-    async updateUser(userId: number, userDto: CreateDto) {
-      let encryptedPassword;
-      try{
-         encryptedPassword = this.encryptPassword(userDto.password)
-      }
-      catch(err){
-        return err;
-      }
-
-      const newUser = await this.prismaService.user.update({
-        where: {
-          id: userId
-        },
-        data: {
-          email: userDto.email,
-          password: encryptedPassword,
-          firstname: userDto.firstname,
-          lastname: userDto.lastname,
-          phone: userDto.phone
-        }
-      });
-      return newUser;
-    }
-
-    async deleteUser(userId: number){
-      try{
-        this.prismaService.user.delete({
-          where: {
-            id: userId
-          }
-        })
-        return `Successfully deleted user with id: ${userId}`
-      }
-      catch(err){
-        return err;
-      }
-    }
-
-
-
+    })
+    return {message: `Successfully deleted user with id: ${userId}`};
+  }
 
 }
