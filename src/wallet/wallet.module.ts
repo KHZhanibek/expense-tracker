97c97bc2306f62e60363d6c37cde89a232e38d88
Zhanibek <zkhodzhabekov@gmail.com>
2023-03-17 19:08:03 +0600

    Add DI for connecting layers in wallet Module connect Data Layer with Service Layer with connect Service Layer with Controller layer
    used Dependency Injection
    
    WALLET MODULE UPDATES

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [WalletService],
   controllers: [WalletController]
 })
--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+  constructor(private prismaSerivce: PrismaService){}
+
+}
