1c63505242c3f49cccc57631202a266819a4d18d
Zhanibek <zkhodzhabekov@gmail.com>
2023-03-23 14:18:03 +0600

    Add getAllwallets and getWallet to WalletService create 2 functions that get Wallet and All Wallets

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -3,6 +3,25 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class WalletService {
-  constructor(private prismaSerivce: PrismaService){}
+  constructor(private prismaService: PrismaService){}
+    
+  async getAllWallets(){
+    return await this.prismaService.wallet.findMany({
+      select: {
+        title: true,
+        description: true,
+        balance: true
+      }
+    });
+  }
+
+  async getWallet(walletId: number){
+    return await this.prismaService.wallet.findFirst({
+      where:{
+        id: walletId
+      }
+    })
+  }
+  
 
 }
