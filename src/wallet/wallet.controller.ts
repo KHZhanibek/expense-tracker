cd765035790407c80675ac7bc8e8e9d16ca4b5f1
Zhanibek <zkhodzhabekov@gmail.com>
2023-03-23 18:10:42 +0600

    Add all functions of wallet endpoint add getAllWallets, getWallet, createWalle, updateWallet, deleteWallet functions also defined HTTP methods to them made with RESTful architecture

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,8 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CreateDto } from './dto/create.dto';
 import { WalletService } from './wallet.service';
 
-@Controller('wallet')
+@Controller('wallets')
 export class WalletController {
   constructor(private walletService: WalletService){}
   
+  @Get()
+  async getAllWallets(){
+    return this.walletService.getAllWallets();
+  }
+
+  @Get(':id')
+  async getWallet(@Param('id', ParseIntPipe) walletId: number){
+    return this.walletService.getWallet(walletId);
+  }
+
+  @Post()
+  async createWallet(@Body() walletDto: CreateDto){
+    return this.createWallet(walletDto);
+  }
+
+  @Put(':id')
+  async updateWallet(@Param(''))
+
 }
