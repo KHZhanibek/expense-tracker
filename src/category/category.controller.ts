d6b298dc70ddd38d6681f1e23ceb29fefaaa39cf
Zhanibek <zkhodzhabekov@gmail.com>
2023-05-06 09:39:10 +0600

    Implement category controller implement category.controller without expense relation

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateDto } from './dto/create.dto';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async getAllCategories(){
+    return this.categoryService.getAllCategories();
+  }
+
+  @Get(':id')
+  async getCategory(@Param('id', ParseIntPipe) categoryId: number){
+    return this.categoryService.getCategory(categoryId);
+  }
+
+  @Post()
+  async createWallet(@Body() categoryDto: CreateDto){
+    return this.categoryService.createCategory(categoryDto);
+  }
+
+  @Put(':id')
+  async updateWallet(@Param('id', ParseIntPipe) categoryId: number, @Body() categoryDto: CreateDto){
+    return this.categoryService.updateCategory(categoryId, categoryDto);
+  }
+
+  @Delete(':id')
+  async deleteWallet(@Param('id', ParseIntPipe) categoryId: number){
+    return this.categoryService.deleteCategory(categoryId);
+  }
+
+  
+}
