5e3305d996cb852a02edbac63229ac8fa30ae137
SSSimur <simur0297@gmail.com>
2023-06-05 00:49:51 +0600

    Adding Query import from @nestjs/common

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginEmailDto, RegisterDto } from './dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
